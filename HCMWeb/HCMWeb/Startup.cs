b50bc1b7d63d8663afeb34ebf9d3c14ee42ffd61
Benjamin Marshalsea <Benjamin Marshalsea>
2016-07-22 13:54:56 +0100

    iHCMWeb setup

---
+++ HCMWeb/HCMWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HCMWeb.Startup))]
+namespace HCMWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
